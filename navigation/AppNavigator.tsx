23d18fa83555b627820b5ed9dc09c658fedbdca5
Karephilaky <johannescv417@gmail.com>
2025-01-22 21:59:07 -0500

    1v

---
+++ navigation/AppNavigator.tsx
@@ -0,0 +1,27 @@
+// navigation/AppNavigator.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screen/HomeScreen';
+import LoginScreen from '../screen/LoginScreen';
+import RegisterScreen from '../screen/RegisterScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
